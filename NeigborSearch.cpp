ad18c726f1eb215b1dc81085f21bfe4f35bb1600
Jiyihang <1361558438@qq.com>
2024-11-17 20:31:39 +0800

    neighbor search done

--- NeigborSearch.cpp
+++ NeigborSearch.cpp
@@ -1,23 +1,62 @@
 #include "NeighborSearch.h"
 
-int NeighborSearch::_compute_index(const std::vector<double>& position)
+int NeighborSearch::_compute_index(const std::vector<double>& position) const
 {
-	return static_cast<int>(position[0] / grid_size) +
-		static_cast<int>(position[1] / grid_size) * 73856093 +
-		static_cast<int>(position[2] / grid_size) * 19349663;
+	int hash_index = static_cast<int>(position[X] / _grid_size) * _hash_prime[0] +
+		static_cast<int>(position[Y] / _grid_size) * _hash_prime[1] +
+		static_cast<int>(position[Z] / _grid_size) * _hash_prime[2];
+
+	return hash_index;
 }
 
 NeighborSearch::NeighborSearch(const std::vector<Particle>& particles, double grid_size)
-	:grid_size(grid_size) 
+	:_grid_size(grid_size)
 {
 	for (int i = 0; i < particles.size(); ++i)
 	{
 		int index = _compute_index(particles[i].position);
-		grid.emplace(index, i);
+		_grid[index].push_back(i);
 	}
 }
 
-void NeighborSearch::find_neighbours(std::vector<int>& neighbor_index, )
+void NeighborSearch::_get_hash_search_range(int (& search_space)[27], int self_hash_index) const
 {
+	int index = 0;
+	for (int dx = -1; dx <= 1; ++dx)
+	{
+		for (int dy = -1; dy <= 1; ++dy)
+		{
+			for (int dz = -1; dz <= 1; ++dz)
+			{
+				search_space[index] = self_hash_index + 
+					dx * _hash_prime[0] +
+					dy * _hash_prime[1] +
+					dz * _hash_prime[2];
+				++index;
+			}
+		}
+	}
+		
+}
 
+std::vector<int> NeighborSearch::find_neighbours(const std::vector<Particle>& particles, int self_index) const
+{
+	std::vector<int> neighbors_index;
+	int self_hash_index = _compute_index(particles[self_index].position);
+	int search_space[27]{};
+	_get_hash_search_range(search_space, self_hash_index);
+	for (auto hash_index : search_space)
+	{
+		if (_grid.find(hash_index) != _grid.end())
+		{
+			for (auto neighbor_i : _grid.at(hash_index))
+			{
+				if (particles[neighbor_i].cal_distance(particles[self_index]) <= _grid_size)
+				{
+					neighbors_index.push_back(neighbor_i);
+				}
+			}
+		}
+	}
+	return neighbors_index;
 }
--- NeighborSearch.h
+++ NeighborSearch.h
@@ -6,15 +6,17 @@
 
 class NeighborSearch
 {
-private:
-	double grid_size;
-	std::unordered_map<int, std::vector<int>> grid;
-
-	int _compute_index(const std::vector<double>& position);
-
 public:
 	NeighborSearch(const std::vector<Particle>& particles, double grid_size);
-	void find_neighbours();
+	std::vector<int> find_neighbours(const std::vector<Particle>& particles, int self_index) const;
+
+private:
+	double _grid_size;
+	static constexpr int _hash_prime[3] = { 1, 73856093, 19349663 };
+	std::unordered_map<int, std::vector<int>> _grid;
+
+	int _compute_index(const std::vector<double>& position) const;
+	void _get_hash_search_range(int(&search_space)[27], int self_hash_index) const;
 };
 
 #endif
