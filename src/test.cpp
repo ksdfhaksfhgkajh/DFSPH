ea8b9d1ba0d73ce6f57aa0458628128b75113d58
kappa <huaxikappa@outlook.com>
2024-12-03 08:32:28 +0800

    add_time_cal

--- src/test.cpp
+++ src/test.cpp
@@ -1,9 +1,17 @@
 #include "DFSPHSolver.cuh"
+#include <chrono>
 
 int main()
 {
-	DFSPHSolver solver("../sphere.ply", "../result");
+    auto start = std::chrono::high_resolution_clock::now();
+
+    DFSPHSolver solver("../sphere.ply", "../result");
 	//DFSPHSolver solver(1000.0, 1000.0);
 	//solver.export_to_ply(".\\particles_1.ply");
 	solver.simulate();
+
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count();
+    std::cout << "Elapsed time: " << duration << " ms\n";
+    return 0;
 }
